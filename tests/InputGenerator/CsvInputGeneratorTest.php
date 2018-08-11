fe0904d6e39476f6c73881d1b73c5d06674b2bff
Sebastian Michaelsen <sebastian@michaelsen.io>
2018-08-11 19:17:29 +0200

    [TEST] Add Test for CsvInputGenerator

---
+++ tests/InputGenerator/CsvInputGeneratorTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Smichaelsen\Caldera\Test\InputGenerator;
+
+use PHPUnit\Framework\TestCase;
+use Smichaelsen\Caldera\InputGenerator\CsvInputGenerator;
+
+class CsvInputGeneratorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function generatesCsvDataWithFieldNamesFromFirstLine()
+    {
+        $csvInputGenerator = new CsvInputGenerator();
+        $csvInputGenerator->setCsvHandle(fopen(__DIR__ . '/test-equestrian.csv', 'r'));
+        $csvInputGenerator->setSkipLeadingLines(2);
+        $generatedArray = [];
+        foreach ($csvInputGenerator->generateInput() as $inputRecord) {
+            $generatedArray[] = $inputRecord;
+        }
+        $expectedArray = [
+            [
+                'Athlete' => 'Gemna Foo',
+                'Horse' => 'Avalon',
+                'Event' => 'Individual Championship Ia',
+                'Score' => '65050',
+                'Rank' => '13',
+            ],
+            [
+                'Athlete' => 'Gemna Foo',
+                'Horse' => 'Avalon',
+                'Event' => 'Individual Freestyle Ia',
+                'Score' => '65800',
+                'Rank' => '12',
+            ],
+            [
+                'Athlete' => 'Laurentia Tan',
+                'Horse' => 'Ruben James 2',
+                'Event' => 'Individual Championship Ia',
+                'Score' => '73650',
+                'Rank' => '3',
+            ],
+            [
+                'Athlete' => 'Laurentia Tan',
+                'Horse' => 'Ruben James 2',
+                'Event' => 'Individual Freestyle Ia',
+                'Score' => '79000',
+                'Rank' => '2',
+            ],
+            [
+                'Athlete' => 'Maximillian Tan',
+                'Horse' => 'Avalon',
+                'Event' => 'Individual Championship Ia',
+                'Score' => '59304',
+                'Rank' => '15',
+            ],
+            [
+                'Athlete' => 'Maximillian Tan',
+                'Horse' => 'Avalon',
+                'Event' => 'Individual Freestyle Ia',
+                'Score' => '62000',
+                'Rank' => '14',
+            ],
+        ];
+        $this->assertEquals($expectedArray, $generatedArray);
+    }
+}
